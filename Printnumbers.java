5d914463e0f27f9d30a35a0d70601f7022364e44
madhumsc9 <135319538+madhumsc9@users.noreply.github.com>
2024-01-03 19:50:56 +0530

    Add files via upload

---
+++ Printnumbers.java
@@ -0,0 +1,27 @@
+package programmsJava;
+
+public class Printnumbers {
+
+	public static void main(String[] args) {
+		
+		//used for loop
+		
+		for(int i=1;i<10;i++) {
+			System.out.print(i);
+		}
+
+		System.out.println("   for loop                 ");
+		
+		//while loop
+		
+		int x=1;
+		while (x<10) {
+			System.out.print(x);
+			x++;
+			
+		}
+		
+		System.out.println(" while loop");
+	}
+
+}
---
+++ printOddnumbers.java
@@ -0,0 +1,20 @@
+package programmsJava;
+
+public class printOddnumbers {
+
+	public static void main(String[] args) {
+		
+		//used for loop
+		System.out.println("for loop  ");
+	
+		for(int i=0;i<10;i++) {
+			if (i % 2==1) {
+			System.out.println(i);
+		}
+
+				
+}
+		
+	}
+
+}
