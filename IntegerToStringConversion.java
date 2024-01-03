93c8f76bf33e9aaece0f015d236f1a0be7d49a14
madhumsc9 <135319538+madhumsc9@users.noreply.github.com>
2024-01-03 15:01:49 +0530

    Add files via upload

---
+++ IntegerToStringConversion.java
@@ -0,0 +1,23 @@
+package programmsJava;
+
+public class IntegerToStringConversion {
+
+	public static void main(String[] args) {
+		
+		int i=100;
+		
+		String s=String.valueOf(i);
+		System.out.println(i+100);
+		
+		float f=874;
+		String s1=String.valueOf(f);
+		System.out.println(s1);
+		
+		double d=8744567.2;
+		String s2=String.valueOf(d);
+		System.out.println(s2);
+		
+
+	}
+
+}
